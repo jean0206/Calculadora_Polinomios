26aa6fa765b2c9da26288c94ef3665a2f237497c
Maykol Giraldo <Maykol Giraldo@Familia>
2019-02-09 15:06:44 -0500

    Agregar un par de parametros

--- Calculadora_Polinomios/src/modelo/Calculadora.java
+++ Calculadora_Polinomios/src/modelo/Calculadora.java
@@ -1,5 +1,22 @@
 package modelo;
 
-public class Calculadora {
+import java.util.ArrayList;
 
+public class Calculadora {
+	
+	
+	private ArrayList<Integer> divisores;
+	
+
+	public Calculadora() {
+		divisores= new ArrayList<Integer>();
+	}
+
+	
+//	public ArrayList<Integer> sacarLosDivisores() {
+//		
+//	}
+	
+	
 }
+
